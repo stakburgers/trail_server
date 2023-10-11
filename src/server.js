3539cb18ca2fe60a95a3c2ce0d6fa3f7b82d51e5
stakburgers <stakburgers@gmail.com>
2023-10-11 17:00:09 +1300

    Update

--- src/server.js
+++ src/server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const { Pool } = require('pg'); // Import the Pool class from pg
 const app = express();
 const port = process.env.PORT || 3000;
+const wkx = require('wkx');
 
 // Create a PostgreSQL connection pool
 // Create a PostgreSQL connection pool
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'postgres',
-  password: 'postgres',
+  database: 'DOCData',
+  password: 'Group03',
   port: 5432,
 });
 
@@ -19,32 +20,25 @@ app.use(express.static('public'));
 // Define a route to fetch trail coordinates
 app.get('/trail_coordinates/:trailName', async (req, res) => {
   const trailName = req.params.trailName;
-
+  var ogc_fid = 69;
+  //if(trailName == 'albany'){ogc_fid = 69;}
+  var s = 'SELECT wkb_geometry FROM docdatatable WHERE ogc_fid = '+[ogc_fid];
   try {
       // Query your database to retrieve the binary geometries based on the trail name
       const result = await pool.query(
-          'SELECT wkb_geometry FROM your_table WHERE trail_name = $1',
-          [trailName]
+          'SELECT wkb_geometry FROM docdatatable WHERE ogc_fid = $1',
+          [ogc_fid]
       );
 
-      // Convert binary geometries to GeoJSON
-      const geoJSONFeatures = result.rows.map((row) => {
+      const geoJSON = result.rows.map((row) => {
           const wktGeometry = row.wkb_geometry;
-          const geoJSONGeometry = wellknown(wktGeometry); // Convert WKT to GeoJSON
+          //would convert here
           return {
-              type: 'Feature',
-              geometry: geoJSONGeometry,
-              properties: {}, // You can add more properties here if needed
+              geometry: wktGeometry,
           };
       });
 
-      // Create a GeoJSON FeatureCollection
-      const geoJSON = {
-          type: 'FeatureCollection',
-          features: geoJSONFeatures,
-      };
-
-      // Respond with the GeoJSON data
+      //Respond with the GeoJSON data
       res.json(geoJSON);
   } catch (error) {
       console.error('Error:', error);
